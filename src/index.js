349bbee0a7a1dc2e095bad5b33523fd19d8666c8
Frank Gerards <info@typo3expertise.de>
2025-06-04 15:11:52 +0200

    Update index.js to adhere to reactjs standards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { PropTypes } from "prop-types";
 
 const styleSheet = {
@@ -7,63 +7,80 @@ const styleSheet = {
   },
   characterCounter: {
     position: "absolute",
+    right: "8px",
+    bottom: "8px",
     fontSize: "12px",
-    fontWeight: "600"
+    fontWeight: "600",
+    background: "white",
+    padding: "2px 6px",
+    borderRadius: "8px",
+    transition: "color 0.2s, font-weight 0.2s"
   }
 };
 
-export default class CharacterCounter extends Component {
-  divRef = React.createRef();
+const CharacterCounter = ({
+  value = '',
+  maxLength = 0,
+  warningThreshold = 30,
+  wrapperStyle = {},
+  characterCounterStyle = {},
+  overrideStyle = false,
+  children
+}) => {
+  const length = value ? value.length : 0;
+  const remaining = maxLength - length;
 
-  componentDidMount() {
-    if (!this.props.overrideStyle) {
-      const { firstChild, lastChild } = this.divRef.current;
-      lastChild.style.left = `${firstChild.clientWidth - 70}px`;
-      lastChild.style.top = `${firstChild.clientHeight / 2 -
-        lastChild.clientHeight / 2 +
-        3}px`;
-      firstChild.style.paddingRight = "75px";
-    }
+  let computedWrapperStyle = { ...styleSheet.wrapper, ...wrapperStyle };
+  let computedCharacterCounterStyle = { ...styleSheet.characterCounter, ...characterCounterStyle };
+
+  if (remaining < 0) {
+    computedCharacterCounterStyle.color = "#e0245e"; // red
+    computedCharacterCounterStyle.fontWeight = 700;
+  } else if (remaining <= warningThreshold) {
+    computedCharacterCounterStyle.color = "#ffad1f"; // orange
+    computedCharacterCounterStyle.fontWeight = 700;
+  } else {
+    computedCharacterCounterStyle.color = "#657786"; // gray
+    computedCharacterCounterStyle.fontWeight = 600;
   }
 
-  render() {
-    const {
-      value,
-      maxLength,
-      wrapperStyle,
-      characterCounterStyle,
-      overrideStyle
-    } = this.props;
-    let computedWrapperStyle = Object.assign(styleSheet.wrapper, wrapperStyle);
-    let computedCharacterCounterStyle = Object.assign(
-      styleSheet.characterCounter,
-      characterCounterStyle
-    );
-    if (overrideStyle) {
-      computedWrapperStyle = wrapperStyle;
-      computedCharacterCounterStyle = characterCounterStyle;
-    }
-    const displayLength = `${value.length}/${maxLength}`;
-    return (
-      <div ref={this.divRef} style={computedWrapperStyle}>
-        {this.props.children}
-        <span style={computedCharacterCounterStyle}>{displayLength}</span>
-      </div>
-    );
+  let display;
+  if (remaining <= warningThreshold) {
+    display = remaining;
+  } else {
+    display = `${length}/${maxLength}`;
   }
-}
+
+  if (overrideStyle) {
+    computedWrapperStyle = wrapperStyle;
+    computedCharacterCounterStyle = characterCounterStyle;
+  }
+
+  return (
+    <div style={computedWrapperStyle}>
+      {children}
+      <span style={computedCharacterCounterStyle}>{display}</span>
+    </div>
+  );
+};
 
 CharacterCounter.propTypes = {
   value: PropTypes.string.isRequired,
   maxLength: PropTypes.number.isRequired,
+  warningThreshold: PropTypes.number,
   wrapperStyle: PropTypes.object,
   characterCounterStyle: PropTypes.object,
-  overrideStyle: PropTypes.bool
+  overrideStyle: PropTypes.bool,
+  children: PropTypes.node
 };
 
 CharacterCounter.defaultProps = {
+  value: '',
+  maxLength: 0,
+  warningThreshold: 30,
   wrapperStyle: {},
   characterCounterStyle: {},
-  overrideStyle: false,
-  maxLength: 0
+  overrideStyle: false
 };
+
+export default CharacterCounter;
